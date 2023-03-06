c80f4c4791a924d0039357ff611d68b1042a7e73
beluram <belenram1996@gmail.com>
2023-03-06 12:27:41 -0300

    greeting

--- PreEntrega1Ramirez/src/App.jsx
+++ PreEntrega1Ramirez/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Navbar from './components/Navbar';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <ItemListContainer greeting="Bienvenidos"/>
     </div>
   );
 }
